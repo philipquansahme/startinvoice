2cabe077c4613adb2d5284c060156aef527a0623
DESKTOP-A6V2T3R\BRIGHT <danielteyelarbi@yahoo.com>
2020-08-15 09:35:16 -0700

    'PATH'

--- misc/path.php
+++ misc/path.php
@@ -11,4 +11,24 @@
             $content_title = 'Dashboard';
             $content_path = 'pages/users_module.php';
             break;
+        case 'users-info':
+            $page_title = 'User Info: Start Invoice';
+            $content_title = 'User Info';
+            $content_path = 'pages/users_info.php';
+            break;
+        case 'manager-info':
+            $page_title = 'Manager Info: Start Invoice';
+            $content_title = 'Manager Info';
+            $content_path = 'pages/manager_info.php';
+            break;
+        case 'business':
+            $page_title = 'Business: Start Invoice';
+            $content_title = 'Business';
+            $content_path = 'pages/business.php';
+            break;
+        case 'billing-officers':
+            $page_title = 'Billing Officers: Start Invoice';
+            $content_title = 'Billing Officers';
+            $content_path = 'pages/billing_officers.php';
+            break;
     }
