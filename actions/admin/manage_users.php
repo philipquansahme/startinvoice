7ae11d98cf1546a9db4b16a9fd6bf63d0613f8d7
philipquansahme <philipquansahme@gmail.com>
2020-08-23 15:33:26 +0000

    Manage Users Logic Begins

--- actions/admin/manage_users.php
+++ actions/admin/manage_users.php
@@ -1,9 +1,13 @@
 <?php
-    require('../../config/db_con.php');
-    require('../../config/db_functions.php');
+    // if (isset($_POST['edit_business'])) {
+        require('../../config/db_con.php');
+        require('../../config/db_functions.php');
 
-    if ($con) {
-        echo "coon";
-    } else {
-        echo "No Fou";
-    }
+        if ($con) {
+            echo "coon";
+        } else {
+            echo "No Fou";
+        }
+    // } else {
+
+    // }
