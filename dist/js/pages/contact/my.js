3682f75eebe1c20fbd7999ec6eb1f702d8d867cd
philipquansahme <philipquansahme@gmail.com>
2020-09-05 13:30:40 +0000

    Add Supplies Working But Buggy

--- dist/js/pages/contact/my.js
+++ dist/js/pages/contact/my.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $('#delete-btn').on('click', function(event) {
-        console.log("Clicked")
+// $(document).ready(function() {
+//     $('#delete-btn').on('click', function(event) {
+//         console.log("Clicked")
 
-        var delete_id = $(this).attr('data-id');
-        console.log(delete_id);
-    })
-});
+//         var delete_id = $(this).attr('data-id');
+//         console.log(delete_id);
+//     })
+// });
 
